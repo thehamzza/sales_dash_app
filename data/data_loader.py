5c6d8a384bed1bc36dbc08b18ae7bff78374e133
Muhammad Hamza <45312947+thehamzza@users.noreply.github.com>
2024-05-03 22:24:18 +0500

    Add files via upload

---
+++ data/data_loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def load_data():
+    return pd.read_excel(r"/Users/hamza/Developer/python_projects/dash_aldi/dataset.xls")
+
+
+# df = pd.read_excel(r"/Users/hamza/Developer/python_projects/dash_aldi/dataset.xls")
+# #print(df)
