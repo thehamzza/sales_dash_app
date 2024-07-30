92d5e301eac32b86375b419e27b87b738056a220
Muhammad Hamza <45312947+thehamzza@users.noreply.github.com>
2024-07-30 11:51:59 +0500

    Update index.py

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ def display_page(pathname):
 
 if __name__ == '__main__':
     #for local server i.e development
-    app.run_server(debug=True)
+    #app.run_server(debug=True)
     
     #for production server    
     app.run_server(debug=False, host='0.0.0.0', port=5000)
